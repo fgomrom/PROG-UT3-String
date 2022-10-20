3f075fb8a1381f3060d513994f47ce7cdd189af8
fgomrom <fgomrom726@g.educaand.es>
2022-10-20 15:11:49 +0200

    Create EjemplosString.java

---
+++ EjemplosString.java
@@ -0,0 +1,15 @@
+public class EjemplosString {
+    public static void main(String[] args) {
+        // Crear una variable String
+        String nombre = "Fran";
+        String apellido = "Gómez";
+        float edad = 18;
+        // Imprimir el valor de la variable
+        //System.out.println("Hola "+s);
+
+        // Imprimir con formato
+        //Hola Fran Gómez, tengo: 18 años
+        System.out.printf("2\\3 = \"  %.2f\"",2/3f);
+
+    }
+}
